af1d97acf678fce2a2b3b52757199713690862b1
Thomas Haugli <thomas.djonne.haugli@gmail.com>
2023-03-27 19:13:26 +0200

    Thomas på en dum ting

--- src/main/java/snake/SnakeController.java
+++ src/main/java/snake/SnakeController.java
@@ -1,17 +1,28 @@
 package snake;
 
+import java.net.URL;
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.ResourceBundle;
 import java.util.Timer;
 import java.util.TimerTask;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
+import javafx.scene.shape.Rectangle;
 
-public class SnakeController {
+public class SnakeController implements Initializable{
     @FXML
     private TextField navn;
 
@@ -33,19 +44,18 @@ public class SnakeController {
         this.drawBoard();
 
 
-
-        /* Timer t = new Timer();
-        t.schedule(new TimerTask() {
-            @Override
-            public void run() {
-            snake.move();
-            System.out.println(snake);
-            spillbrett.getChildren().clear();
-            XYvalue part = snake.getSnakeBody().get(2);
-            Node node = part.getRectangle();
-            spillbrett.add(node, part.getxValue(), part.getyValue());
-            }
-        }, 1000, 500); */
+        // Timer t = new Timer();
+        // t.schedule(new TimerTask() {
+        //     @Override
+        //     public void run() {
+        //     snake.move();
+        //     System.out.println(snake);
+        //     spillbrett.getChildren().clear();
+        //     XYvalue part = snake.getSnakeBody().get(2);
+        //     Node node = part.getRectangle();
+        //     spillbrett.add(node, part.getxValue(), part.getyValue());
+        //     }
+        // }, 1000, 500); 
 
     }
 
@@ -77,6 +87,25 @@ public class SnakeController {
     private void down() {
         snake.changeDirection("down");
     }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+        Game new_game = new Game();
+        this.snake = new_game.getSnake();
+        this.drawBoard();
+
+        Timeline klokke = new Timeline(new KeyFrame(Duration.ofSeconds(1), e -> {
+            snake.move();
+            this.drawBoard();
+        }));
+
+
+        klokke.setCycleCount(Animation.INDEFINITE);
+        klokke.play();
+    }
+
+
 
     // noe som sjekker hver gang slangen flytter seg
     // swiper hele brettet og fargerlegge det som skal fargelegges på nytt
