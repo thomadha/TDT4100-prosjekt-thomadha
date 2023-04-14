a2ee923e92edb806eebb14ecc3ae772237f85788
amaliejvik <amaligjo@stud.ntnu.no>
2023-04-14 12:03:29 +0200

    Prøvde å ta testene ut av exampleproject

--- src/test/java/Snake/AppleTest.java
+++ src/test/java/Snake/AppleTest.java
@@ -1,4 +1,4 @@
-package exampleproject.Snake;
+package Snake;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
--- src/test/java/Snake/FileTesting.java
+++ src/test/java/Snake/FileTesting.java
@@ -1,4 +1,4 @@
-package exampleproject.Snake;
+package Snake;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
